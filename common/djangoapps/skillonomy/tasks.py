d8601709ff0c1eab416e2e35e0f78a0058a0f32e
Alexander Marenich <sendr84@gmail.com>
2018-11-09 22:11:16 +0200

    caching events to avoid double sending

--- common/djangoapps/skillonomy/tasks.py
+++ common/djangoapps/skillonomy/tasks.py
@@ -6,6 +6,7 @@ from xmodule.modulestore.django import modulestore
 from opaque_keys.edx.keys import CourseKey
 import requests
 from urlparse import urljoin
+from django.core.cache import cache
 
 
 log = logging.getLogger(__name__)
@@ -17,11 +18,18 @@ def send_api_request(data):
     Send data to Skillonomy API
     """
     base_url = urljoin(data.get('base_url'), data.get('path', ''))
+    cache_key = '{}:{}:{}'.format(
+        data.get('payload', {}).get('student_id'),
+        data.get('payload', {}).get('course_id'),
+        data.get('payload', {}).get('event_type')
+    )
 
     log.info("Data to be sent - {}".format(data))
-
-    response = requests.post(base_url, data=data.get('payload'), headers={
-        "HTTP_SECRET": data.get("secret"),
-        "HTTP_KEY": data.get("key")
-    })
-    log.info("Skillonomy response: status - {}, content - {}".format(response.status_code, response.content))
+    if not cache.get(cache_key):
+        response = requests.post(base_url, data=data.get('payload'), headers={
+            "HTTP_SECRET": data.get("secret"),
+            "HTTP_KEY": data.get("key")
+        })
+        if response.status_code == 200:
+            cache.set(cache_key, True, 30)
+        log.info("Skillonomy response: status - {}, content - {}".format(response.status_code, response.content))
